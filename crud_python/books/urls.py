417598d0f844e0c89aa3379ca578d74bd0e92b85
iRuperth <ruperthlosada@gmail.com>
2026-01-12 14:55:05 +0100

    fix: fixing urls

--- crud_python/books/urls.py
+++ crud_python/books/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
-    path('<int:book_id>/', views.detail, name='detail'),
+    path('<int:book_id>/', views.detail, name='book_detail'), 
     path('create/', views.create, name='create'),
     path('<int:book_id>/edit/', views.update_book, name='update_book'),
     path('<int:book_id>/delete/', views.delete_book, name='delete_book'),
